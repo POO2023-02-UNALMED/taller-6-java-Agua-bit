b49a8240995fa95968c61affdc7baedcea0f8cde
= <60821161+Agua-bit@users.noreply.github.com>
2023-10-03 01:54:39 -0500

    Update Fabricante.java

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -6,6 +6,7 @@ public class Fabricante {
     private String nombre;
     private Pais pais;
     private static List<Fabricante>fabricantes = new ArrayList<>();
+    private int numeroVehi;
 
     public  Fabricante(String nombre, Pais pais){
         Fabricante.fabricantes.add(this);
@@ -30,6 +31,25 @@ public class Fabricante {
     }
 
     public String fabricaMayorVentas(){
+        int a;
+        for (int a=0, a < fabricantes.size()-1, a++){
 
+        }
+    }
+
+    public static List<Fabricante> getFabricantes() {
+        return fabricantes;
+    }
+
+    public static void setFabricantes(List<Fabricante> fabricantes) {
+        Fabricante.fabricantes = fabricantes;
+    }
+
+    public int getNumeroVehi() {
+        return numeroVehi;
+    }
+
+    public void setNumeroVehi(int numeroVehi) {
+        this.numeroVehi = numeroVehi;
     }
 }
