a69efc19f2fe5a608bf79bc645884fbc1a8b10b1
= <60821161+Agua-bit@users.noreply.github.com>
2023-10-03 02:48:27 -0500

    Update Automovil.java

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -4,7 +4,7 @@ public class Automovil extends Vehiculo{
     private int puestos;
     private static int cifraAutomoviles;
 
-    public Automovil(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, int puestos){
+    public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos){
         super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
         this.puestos = puestos;
         Automovil.cifraAutomoviles++;
